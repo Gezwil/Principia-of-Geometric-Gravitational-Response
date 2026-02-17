62167505f2e12ecb23fce4d3d72b05a5258b99ba
Gezwil Percy <academicnode14@gmail.com>
2026-02-17 09:20:56 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,23 @@
+from .geometric_bridge import (
+    predict_rotation_curve,
+    g_obs_from_g_bar,
+    v_bar_from_components,
+    boost,
+    halo_boundary_radius,
+    baryonic_mass_from_btfr,
+    rms_residual,
+    r_squared,
+    A0,
+)
+
+__all__ = [
+    'predict_rotation_curve',
+    'g_obs_from_g_bar',
+    'v_bar_from_components',
+    'boost',
+    'halo_boundary_radius',
+    'baryonic_mass_from_btfr',
+    'rms_residual',
+    'r_squared',
+    'A0',
+]
